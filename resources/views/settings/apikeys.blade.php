48d511607c6092922e14df0c03d12ae23eed725a
Kamaro Lambert <kamaroly@gmail.com>
2016-02-22 13:49:11 +0200

    USER ID for only the logged in session

--- resources/views/settings/apikeys.blade.php
+++ resources/views/settings/apikeys.blade.php
@@ -12,12 +12,12 @@
     <tr>
         <td>{{ trans('settings.test_key') }}</td>
         <td>{!! $apikeys->testKey !!}</td>
-        <td><a href="{{ route('settings.keys.generate',['environment'=>'test','userid'=>$apikeys->user_id]) }}" class="btn btn-info">refresh</a></td>
+        <td><a href="{{ route('settings.keys.generate',['environment'=>'test','userid'=>\Sentry::getUser()->id]) }}" class="btn btn-info">refresh</a></td>
     </tr>
     <tr>
         <td>{{ trans('settings.live_key') }}</td>
         <td>{!! $apikeys->liveKey !!}</td>
-         <td><a href="{{ route('settings.keys.generate',['environment'=>'live','userid'=>$apikeys->user_id]) }}" class="btn btn-info">refresh</a></td>
+         <td><a href="{{ route('settings.keys.generate',['environment'=>'live','userid'=>\Sentry::getUser()->id]) }}" class="btn btn-info">refresh</a></td>
     </tr>
 </table>
 
