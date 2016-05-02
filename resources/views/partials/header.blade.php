77be48f06fa418bfed19dd6827d098c16944a3fb
Kamaro Lambert <kamaroly@gmail.com>
2016-05-02 22:22:51 +0200

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="google-site-verification" content="mfGlZGn127qk5Qu0rhg3icB8S4yzsDiPGUOxFDGyTQ4" />
     <link rel="icon" href="http://tripexpress.app/images/favicon.ico">
     <title>@yield('title')</title>  
 <!-- CSS -->
